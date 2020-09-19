6aebbef1ba19b6f5803e1e48f8599d00976fb451
Sai Srihitha Reddy.T <42039283+SrihithaReddy13@users.noreply.github.com>
2020-09-19 13:10:02 +0530

    Add files via upload

--- js/student_register.js
+++ js/student_register.js
@@ -1,10 +1,42 @@
 var check = function() {
     if (document.getElementById('pwd').value ==
       document.getElementById('cpwd').value) {
-      document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = 'matching';
+      document.getElementById('match_pwd').style.color = 'green';
+      document.getElementById('match_pwd').innerHTML = 'matching';
     } else {
-      document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = 'not matching';
+      document.getElementById('match_pwd').style.color = 'red';
+      document.getElementById('match_pwd').innerHTML = 'not matching';
     }
+}
+
+var name_check = function(){
+  var letters = /^[A-Za-z\s]+$/;
+  var name=document.getElementById("stud_name");
+  if (name.value==""){
+    document.getElementById('name_val').style.display = 'none'; 
+  }
+  else if(name.value.match(letters)){
+    document.getElementById('name_val').style.display = 'none'; 
+  }
+  else{
+    document.getElementById('name_val').style.display = 'block';  
+    document.getElementById('name_val').style.color = 'red';
+    document.getElementById('name_val').innerHTML = 'This field only allows alphabets';
+  }
+}
+
+var rno_check = function(){
+  var rollno_format = /^([C][B]|[A][M]|[B][G]).([E][N]).([U][4]|[M][3])([C][S][E]|[E][E][E]|[E][C][E]|[M][E][C]|[C][I][V])([1][6]|[1][7]|[1][8])([0-9]{3})$/;
+  var rno=document.getElementById("rollno");
+  if (rno.value==""){
+    document.getElementById('roll_val').style.display = 'none'; 
+  }
+  else if(rno.value.match(rollno_format)){
+    document.getElementById('roll_val').style.display = 'none'; 
+  }
+  else{
+    document.getElementById('roll_val').style.display = 'block';  
+    document.getElementById('roll_val').style.color = 'red';
+    document.getElementById('roll_val').innerHTML = 'Please Enter correct format of Roll number (ex: CB.EN.U4CSE170XX)';
+  }
 }
--- js/student_register_test.js
+++ js/student_register_test.js
@@ -8,7 +8,7 @@ describe('passwordMatch Directive - ', function() {
         return HTMLElements[ID];
     });
     it('should indicate "not matching" when the passwords do not match.', function() {
-        var msg=document.getElementById("message");
+        var msg=document.getElementById("match_pwd");
         var pwd1=document.getElementById("pwd");
         var pwd2=document.getElementById("cpwd");
         pwd1.setAttribute("value","123");
@@ -17,7 +17,7 @@ describe('passwordMatch Directive - ', function() {
         expect(msg.innerHTML).toEqual("not matching");
     });
     it('should change the text to red color when the passwords do not match.', function() {
-        var msg=document.getElementById("message");
+        var msg=document.getElementById("match_pwd");
         var pwd1=document.getElementById("pwd");
         var pwd2=document.getElementById("cpwd");
         pwd1.setAttribute("value","123");
@@ -26,7 +26,7 @@ describe('passwordMatch Directive - ', function() {
         expect(msg.style.color).toEqual("red");
     });
     it('should indicate "matching" when the passwords match.', function() {
-        var msg=document.getElementById("message");
+        var msg=document.getElementById("match_pwd");
         var pwd1=document.getElementById("pwd");
         var pwd2=document.getElementById("cpwd");
         pwd1.setAttribute("value","1234");
@@ -35,12 +35,41 @@ describe('passwordMatch Directive - ', function() {
         expect(msg.innerHTML).toEqual("matching");
     });
     it('should change the text to green color when the passwords match.', function() {
-        var msg=document.getElementById("message");
+        var msg=document.getElementById("match_pwd");
         var pwd1=document.getElementById("pwd");
         var pwd2=document.getElementById("cpwd");
         pwd1.setAttribute("value","1234");
         pwd2.setAttribute("value","1234");
         var calc = new check();
         expect(msg.style.color).toEqual("green");
+    });
+    it('should display error message when name is not comprised of only alphabets', function(){
+        var msg=document.getElementById("name_val");
+        var name=document.getElementById("stud_name");
+        name.setAttribute("value","srih2343");
+        var calc=new name_check();
+        expect(msg.style.display).toEqual("block");
+    });
+    it('should display no error message when name is comprised of only alphabets', function(){
+        var msg=document.getElementById("name_val");
+        var name=document.getElementById("stud_name");
+        name.setAttribute("value","Srihitha Reddy");
+        var calc=new name_check();
+        expect(msg.style.display).toEqual("none");
+    });
+    it('should display error message when roll no is not in format',function(){
+        var msg=document.getElementById("roll_val");
+        var rno=document.getElementById("rollno");
+        rno.setAttribute("value","abcdefgh");
+        var calc=new rno_check();
+        expect(msg.style.display).toEqual("block");
+    });
+    
+    it('should display no message when roll no is in format',function(){
+        var msg=document.getElementById("roll_val");
+        var rno=document.getElementById("rollno");
+        rno.setAttribute("value","CB.EN.U4CSE17065");
+        var calc=new rno_check();
+        expect(msg.style.display).toEqual("none");
     });
 });
